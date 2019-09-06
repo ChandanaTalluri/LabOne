280660fe163bb49ecf71ea7b8fc5e19c47224874
Chandana <Chandana@DESKTOP-3H97OB6>
2019-09-05 23:07:55 -0500

    NewCode

--- src/main/java/LabOne/QuestionEight.java
+++ src/main/java/LabOne/QuestionEight.java
@@ -15,14 +15,14 @@ public class QuestionEight {
     public static void main(String[] args) {
         //40 cookies ,10 servings which mean each serving has 4 cookies
         //4 cookies => 300 calories
-        // x number of cookies
+        // x number of cookies = (x*300)/4
         //each bag has 40 cookies
          
         String cookies = JOptionPane.showInputDialog("Please enter the amount :");
 
         try {
             float  noOfcookies =  Float.parseFloat(cookies);
-            float noOfCalaries = (noOfcookies * 30)/40;
+            float noOfCalaries = (noOfcookies * 300)/4;
             String strRounded = String.format("%.2f",noOfCalaries);
             JOptionPane.showMessageDialog(null, "Number of calories consumed :"+strRounded);
             System.out.println("Number of calories consumed :"+strRounded);
